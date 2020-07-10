a7132028072604ddc7c61c59a114fcc67c67431a
AlexLee <alexlee.deu@gmail.com>
2020-07-10 22:39:16 +0200

    add engine mat library

--- optimal_convex_partition_2/cmake-build-debug/CMakeFiles/optimal_convex_partition_2.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for optimal_convex_partition_2.
-# This may be replaced when dependencies are built.
--- optimal_convex_partition_2/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alxlee/Documents/TEST_CODE/Cgal_matlab/CGAL-matlab/optimal_convex_partition_2
-# Build directory: /home/alxlee/Documents/TEST_CODE/Cgal_matlab/CGAL-matlab/optimal_convex_partition_2/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
